bad17885836b92f1926a4be10f08f637e7f2f1a5
Zilvinas Bucys <zilbuc@gmail.com>
2019-01-14 00:18:19 +0200

    added /ping and /users

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;  // is used to get the payload
-const config = require('./config');
+const config = require('./lib/config');
 const fs = require('fs'); // file system module
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
 // Testing CRUD:
 // var _data = require('./lib/data');
@@ -84,7 +86,7 @@ const unifiedServer = (req, res) => {
       'queryStringObject' : queryStringObject,
       'method' : method,
       'headers' : headers,
-      'payload' : buffer
+      'payload' : helpers.parseJsonToObject(buffer)
     };
 
     // Route the request to the handler specified in the router
@@ -116,22 +118,8 @@ const unifiedServer = (req, res) => {
   });
 };
 
-// Define the handlers
-let handlers = {};
-
-// Ping handler
-handlers.ping = (data, callback) => {
-  // Callback a http status code and a payload object
-  callback(200);
-};
-
-// 'Not found' handler
-handlers.notFound = (data, callback) => {
-  callback(404);  // no payload
-};
-
 // Define a request router
 let router = {
-  'sample' : handlers.sample,
-  'ping' : handlers.ping
+  'ping' : handlers.ping,
+  'users' : handlers.users
 };
