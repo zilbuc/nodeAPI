d32966c4beb6ab4640d32ba55e0d30e364a20b5c
Zilvinas Bucys <zilbuc@gmail.com>
2019-03-28 21:16:04 +0200

    added AJAX requests for API calls

--- public/app.js
+++ public/app.js
@@ -3,6 +3,82 @@
  *
  */
 
+// Container for the frontend application
 const app = {};
 
-console.log('Hello console world!');
+// Config
+app.config = {
+  'sessionToken' : false
+};
+
+// AJAX client for the restful API
+app.client = {};
+
+// Interface for making API calls
+app.client.request = (headers, path, method, queryStringObject, payload, callback) => {
+
+  // Set the defaults
+  headers = typeof(headers) === 'object' && headers !== null ? headers : {};
+  path = typeof(path) === 'string' ? path : '/';
+  method = typeof(method) === 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method) > -1 ? method.toUpperCase() : 'GET';
+  queryStringObject = typeof(queryStringObject) === 'object' && queryStringObject !== null ? queryStringObject : {};
+  payload = typeof(payload) === 'object' && payload !== null ? payload : {};
+  callback = typeof(callback) === 'function' ? callback : false; // to allow making requests without callback
+
+  // For each query string parameter sent, add it to the path
+  let requestUrl = path+'?';
+  let counter = 0;
+  for (let queryKey in queryStringObject) {
+    if (queryStringObject.hasOwnProperty(queryKey)) {
+      counter++;
+      // If at least one query string parameter has already been added, prepend new ones with an ampersand
+      if (counter > 1) {
+        requestURL+='&';
+      }
+      // Add the key value
+      requestUrl+=queryKey+'='+queryStringObject[queryKey];
+    }
+  }
+
+  // Form the HTTP request as a JSON type
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, requestUrl, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // For each header sent, add it to the request
+  for (let headerKey in headers) {
+    if (headers.hasOwnProperty(headerKey)) {
+      xhr.setRequestHeader(headerKey, headers[headerKey]);
+    }
+  }
+
+  // If there is a current session token set, add that as a header
+  if (app.config.sessionToken) {
+    xhr.setRequestHeader("token", app.config.sessionToken.id);
+  }
+
+  // When the request comes back, handle the response
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      const statusCode = xhr.status;
+      const responseReturned = xhr.responseText;
+
+      // Callback if requested
+      if (callback) {
+        try {
+          const parsedResponse = JSON.parse(responseReturned);
+          callback(statusCode, parsedResponse);
+        } catch(e) {
+          callback(statusCode, false);
+        }
+      }
+    }
+  }
+
+  // Set the payload as JSON
+  const payloadString = JSON.stringify(payload);
+  xhr.send(payloadString);
+
+};
+
+// To test app in browser console: app.client.request(undefined, '/ping','GET', undefined, undefined, function(statusCode, payload){console.log(statusCode, payload);});
